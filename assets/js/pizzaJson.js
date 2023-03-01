a7dca2e43621447ab0ac0a6abce80980814f56a2
Vinicius Koga <viniciuskoga_m@hotmail.com>
2023-03-01 19:27:24 -0300

    fixed images display bug

--- assets/js/pizzaJson.js
+++ assets/js/pizzaJson.js
@@ -1,10 +1,10 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'../assets/img/pizza01.jpg', price:[20.19, 27.50, 38.75], sizes:['100g', '530g', '860g'], description:'Tomate seco, Mussarela & calabresa'},
-    {id:2, name:'Calabresa', img:'../assets/img/pizza02.jpg', price:[18.00, 25.50, 32.75], sizes:['320g', '530g', '860g'], description:'Mussarela, pimentão, palmito & parmesão'},
-    {id:3, name:'Australiana', img:'../assets/img/pizza03.jpg', price:[17.45, 27.50, 39.75], sizes:['320g', '530g', '860g'], description:'Atum, tomate seco & parmesão'},
-    {id:4, name:'Americana', img:'../assets/img/pizza04.jpg', price:[19.77, 24.50, 37.75], sizes:['320g', '530g', '860g'], description:'Pepperonni, mussarela, parmesão & brocolis'},
-    {id:5, name:'Portuguesa', img:'../assets/img/pizza05.jpg', price:[21.43, 26.50, 35.75], sizes:['320g', '530g', '860g'], description:'Parmesão, tomate, calabresa & mussarela'},
-    {id:6, name:'Atum', img:'../assets/img/pizza06.jpg', price:[18.55, 23.50, 38.75], sizes:['320g', '530g', '860g'], description:'Calabresa moida, pepperonni, brocolis & palmito'},
+    {id:1, name:'Mussarela', img:'./assets/img/pizza01.jpg', price:[20.19, 27.50, 38.75], sizes:['100g', '530g', '860g'], description:'Tomate seco, Mussarela & calabresa'},
+    {id:2, name:'Calabresa', img:'./assets/img/pizza02.jpg', price:[18.00, 25.50, 32.75], sizes:['320g', '530g', '860g'], description:'Mussarela, pimentão, palmito & parmesão'},
+    {id:3, name:'Australiana', img:'./assets/img/pizza03.jpg', price:[17.45, 27.50, 39.75], sizes:['320g', '530g', '860g'], description:'Atum, tomate seco & parmesão'},
+    {id:4, name:'Americana', img:'./assets/img/pizza04.jpg', price:[19.77, 24.50, 37.75], sizes:['320g', '530g', '860g'], description:'Pepperonni, mussarela, parmesão & brocolis'},
+    {id:5, name:'Portuguesa', img:'./assets/img/pizza05.jpg', price:[21.43, 26.50, 35.75], sizes:['320g', '530g', '860g'], description:'Parmesão, tomate, calabresa & mussarela'},
+    {id:6, name:'Atum', img:'./assets/img/pizza06.jpg', price:[18.55, 23.50, 38.75], sizes:['320g', '530g', '860g'], description:'Calabresa moida, pepperonni, brocolis & palmito'},
 ]
 
 export {pizzaJson};
