01f85e3ed936a6ed261759f44516bfdb2f0d5e86
Vinicius Koga <viniciuskoga_m@hotmail.com>
2023-03-02 21:01:08 -0300

    improved responsiveness & changed base design

--- assets/js/pizzaJson.js
+++ assets/js/pizzaJson.js
@@ -1,10 +1,13 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'./assets/img/pizza01.jpg', price:[20.19, 27.50, 38.75], sizes:['100g', '530g', '860g'], description:'Tomate seco, Mussarela & calabresa'},
-    {id:2, name:'Calabresa', img:'./assets/img/pizza02.jpg', price:[18.00, 25.50, 32.75], sizes:['320g', '530g', '860g'], description:'Mussarela, pimentão, palmito & parmesão'},
-    {id:3, name:'Australiana', img:'./assets/img/pizza03.jpg', price:[17.45, 27.50, 39.75], sizes:['320g', '530g', '860g'], description:'Atum, tomate seco & parmesão'},
-    {id:4, name:'Americana', img:'./assets/img/pizza04.jpg', price:[19.77, 24.50, 37.75], sizes:['320g', '530g', '860g'], description:'Pepperonni, mussarela, parmesão & brocolis'},
-    {id:5, name:'Portuguesa', img:'./assets/img/pizza05.jpg', price:[21.43, 26.50, 35.75], sizes:['320g', '530g', '860g'], description:'Parmesão, tomate, calabresa & mussarela'},
-    {id:6, name:'Atum', img:'./assets/img/pizza06.jpg', price:[18.55, 23.50, 38.75], sizes:['320g', '530g', '860g'], description:'Calabresa moida, pepperonni, brocolis & palmito'},
+    {id:1, name:'Mussarela', img:'./assets/img/pizza01.jpg', price:[20.99, 27.99, 38.99], sizes:['100g', '530g', '860g'], description:'Descrição da pizza contendo ingredientes.'},
+    {id:2, name:'Calabresa', img:'./assets/img/pizza02.jpg', price:[18.99, 25.99, 32.99], sizes:['320g', '530g', '860g'], description:'Descrição da pizza contendo ingredientes.'},
+    {id:3, name:'Australiana', img:'./assets/img/pizza03.jpg', price:[17.99, 27.99, 39.99], sizes:['320g', '530g', '860g'], description:'Descrição da pizza contendo ingredientes.'},
+    {id:4, name:'Americana', img:'./assets/img/pizza04.jpg', price:[19.99, 24.99, 37.99], sizes:['320g', '530g', '860g'], description:'Descrição da pizza contendo ingredientes.'},
+    {id:5, name:'Portuguesa', img:'./assets/img/pizza05.jpg', price:[21.99, 26.99, 35.99], sizes:['320g', '530g', '860g'], description:'Descrição da pizza contendo ingredientes.'},
+    {id:6, name:'Atum', img:'./assets/img/pizza06.jpg', price:[18.99, 23.99, 38.99], sizes:['320g', '530g', '860g'], description:'Descrição da pizza contendo ingredientes.'},
+    {id:7, name:'Vegetariana', img:'./assets/img/pizza07.jpg', price:[20.99, 28.99, 38.99], sizes:['320g', '530g', '860g'], description:'Descrição da pizza contendo ingredientes.'},
+    {id:8, name:'Brocolis', img:'./assets/img/pizza08.jpg', price:[25.99, 35.99, 45.99], sizes:['320g', '530g', '860g'], description:'Descrição da pizza contendo ingredientes.'},
+    {id:9, name:'Mexicana', img:'./assets/img/pizza09.jpg', price:[30.99, 35.99, 45.99], sizes:['320g', '530g', '860g'], description:'Descrição da pizza contendo ingredientes.'},
 ]
 
 export {pizzaJson};
